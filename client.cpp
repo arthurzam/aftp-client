417d8ab790a94130d47cbe102a34c3dfc45b7bfd
Arthur Zamarin <arthurzam@gmail.com>
2015-02-20 15:11:10 +0200

    Big fix
    fix endian for file size
    fix upload & download
    fix symlink

--- client.cpp
+++ client.cpp
@@ -19,15 +19,21 @@
 #include "messages.h"
 
 #define DEFAULT_PORT 7777
-#define DEFAULT_HOST "localhost"
+#define DEFAULT_HOST (char*)"localhost"
 #define BUFFER_SERVER_SIZE 0x400 // = 1024
+
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+#define bswap64(y) (((uint64_t)ntohl(y)) << 32 | ntohl(y>>32))
+#else
+#define bswap64(y) (y)
+#endif
 
 SOCKET sock;
 
 int sendMessage(short msgCode, char* data, int datalen);
 short getMsgCode(char* data, unsigned int datalen);
 void uploadFile(FILE* file);
-void downloadFile(FILE* file, int blockCount);
+void downloadFile(FILE* file, uint32_t blockCount);
 
 int main(int argc, char* argv[])
 {
@@ -122,6 +128,7 @@ int main(int argc, char* argv[])
         switch (msgCode)
         {
         case CLIENT_MSG::SERVER_INFO:
+        case CLIENT_MSG::CLIENT_INFO:
         case CLIENT_MSG::LOGOUT:
         case CLIENT_MSG::DIR_PWD:
             len = 0;
@@ -146,9 +153,12 @@ int main(int argc, char* argv[])
             fseek(fileTemp, 0, SEEK_END);
             tempdata.l = ftell(fileTemp);
             fseek(fileTemp, 0, SEEK_SET);
-            *(int*)(Buffer) = htonl((tempdata.l / 0x200));
-            if(*(int*)(Buffer) * 0x200 < tempdata.l)
-                *(int*)(Buffer) = htonl(1 + *(int*)(Buffer));
+            {
+                uint32_t count = (uint32_t)(tempdata.l / 0x200);
+                if(count * 0x200 < tempdata.l)
+                    ++count;
+                *(uint32_t*)(Buffer) = htonl(count);
+            }
             printf("enter remote path: ");
             scanf("%s", Buffer + 4);
             break;
@@ -171,6 +181,7 @@ int main(int argc, char* argv[])
         case CLIENT_MSG::FILE_COPY:
         case CLIENT_MSG::DIR_MOVE:
         case CLIENT_MSG::DIR_COPY:
+        case CLIENT_MSG::FILE_SYMLINK:
             printf("enter source relative path: ");
             scanf("%s", Buffer + 1); // src
             *Buffer = strlen(Buffer + 1) & 0xFF; // src_len
@@ -188,14 +199,11 @@ int main(int argc, char* argv[])
         }
         if (sendMessage(msgCode, Buffer, len) == SOCKET_ERROR)
         {
-            fprintf(stderr,"Client: send() failed.\n");
-            goto _badExit;
+            goto _badSend;
         }
         if ((retval = recv(sock, Buffer, BUFFER_SERVER_SIZE, 0)) == SOCKET_ERROR)
         {
-            fprintf(stderr,"Client: recv() failed.\n");
-            closesocket(sock);
-            goto _badExit;
+            goto _badRecv;
         }
         Buffer[retval] = 0;
         tempdata.i = getMsgCode(Buffer, retval);
@@ -223,32 +231,30 @@ int main(int argc, char* argv[])
             for(tempdata.i = 2; tempdata.i < 18; tempdata.i++)
                 printf("%02X", ((uint8_t*)Buffer)[tempdata.i]);
             printf("\n");
+            if ((retval = recv(sock, Buffer, BUFFER_SERVER_SIZE, 0)) == SOCKET_ERROR)
+                goto _badRecv;
             continue;
         }
         else if(msgCode == CLIENT_MSG::FILE_SIZE && tempdata.i == SERVER_MSG::ACTION_COMPLETED)
         {
-            // TODO: fix uint64_t endian
-            tempdata.l = *((uint64_t*)(Buffer + 2));
+            tempdata.l = bswap64(*((uint64_t*)(Buffer + 2)));
 #ifdef WIN32
             printf("File size is %I64u\n", tempdata.l);
 #else
             printf("File size is %llu\n", tempdata.l);
 #endif
+            if ((retval = recv(sock, Buffer, BUFFER_SERVER_SIZE, 0)) == SOCKET_ERROR)
+                goto _badRecv;
             continue;
         }
         else if(tempdata.i == SERVER_MSG::TIMEOUT)
         {
             if ((retval = sendMessage(CLIENT_MSG::EMPTY_MESSAGE, NULL, 0)) == SOCKET_ERROR)
             {
-                fprintf(stderr,"Client: send() failed.\n");
-                goto _badExit;
+                goto _badSend;
             }
             if ((retval = recv(sock, Buffer, BUFFER_SERVER_SIZE, 0)) == SOCKET_ERROR)
-            {
-                fprintf(stderr,"Client: recv() failed.\n");
-                closesocket(sock);
-                goto _badExit;
-            }
+                goto _badRecv;
             continue;
         }
         while(tempdata.i == SERVER_MSG::LS_DATA)
@@ -256,9 +262,7 @@ int main(int argc, char* argv[])
             printf("%s", Buffer + sizeof(msgCode));
             if ((retval = recv(sock, Buffer, BUFFER_SERVER_SIZE, 0)) == SOCKET_ERROR)
             {
-                fprintf(stderr,"Client: recv() failed.\n");
-                closesocket(sock);
-                goto _badExit;
+                goto _badRecv;
             }
             tempdata.i = getMsgCode(Buffer, retval);
         }
@@ -271,6 +275,13 @@ int main(int argc, char* argv[])
 #endif
     return (0);
 
+_badRecv:
+    fprintf(stderr,"Client: recv() failed.\n");
+    closesocket(sock);
+    goto _badExit;
+_badSend:
+    fprintf(stderr,"Client: send() failed.\n");
+    goto _badExit;
 _badExit:
 #ifdef WIN32
     WSACleanup();
@@ -309,39 +320,47 @@ short getMsgCode(char* data, unsigned int datalen)
 
 void uploadFile(FILE* file)
 {
-    struct {
+    struct __attribute__((packed)){
         uint32_t blockNum;
         uint16_t size;
         uint8_t md5Res[16];
         uint8_t dataFile[0x200];
     } data;
-    char Buffer[BUFFER_SERVER_SIZE];
+    struct __attribute__((packed)){
+        uint16_t msgCode;
+        union __attribute__((packed)){
+            char data[BUFFER_SERVER_SIZE - sizeof(msgCode)];
+            uint32_t completedBlock;
+        }u;
+    } Buffer;
     int blocksCount, i;
     uint8_t* blocks; // 1 - bad, 0 - good
     int flag = 1;
-    for(data.blockNum = 0; (data.size = fread(data.dataFile, 1, 0x200, file)); data.blockNum++)
+    size_t readBytes;
+    uint32_t blockNum;
+    for(blockNum = 0; (readBytes = fread(data.dataFile, 1, 0x200, file)); ++blockNum)
     {
-        MD5(data.dataFile, data.size, data.md5Res);
-        data.blockNum = htonl(data.blockNum);
-        data.size = htons(data.size);
-        sendMessage(CLIENT_MSG::FILE_BLOCK, (char*)&data, 32 + data.size);
-        data.blockNum = ntohl(data.blockNum);
+        MD5(data.dataFile, readBytes, data.md5Res);
+        data.blockNum = htonl(blockNum);
+        data.size = htons(readBytes);
+        sendMessage(CLIENT_MSG::FILE_BLOCK, (char*)&data, 22 + readBytes);
     }
-    blocksCount = data.blockNum;
+    blocksCount = blockNum;
     blocks = (uint8_t*)malloc(blocksCount);
     memset(blocks, 1, blocksCount);
     while(flag)
     {
-        if(getMsgCode(Buffer, recv(sock, Buffer, BUFFER_SERVER_SIZE, 0)) == SERVER_MSG::ACTION_COMPLETED)
+        recv(sock, (char*)&Buffer, BUFFER_SERVER_SIZE, 0);
+        if(ntohs(Buffer.msgCode) == SERVER_MSG::ACTION_COMPLETED)
         {
-            blocks[(int)*(Buffer + 2)] = 0;
+            blocks[ntohl(Buffer.u.completedBlock)] = 0;
             flag = 0;
-            for(i = 0; !flag && i < blocksCount; i++)
+            for(i = 0; !flag || i < blocksCount; i++)
                 flag |= blocks[i]; // only if the whole array it 0 (we finished) than flag will be 0
         }
         else
         {
-            data.blockNum = *(Buffer + 2);
+            data.blockNum = ntohl(Buffer.u.completedBlock);
             fseek(file, data.blockNum * 0x200, SEEK_SET);
             data.size = fread(data.dataFile, 1, 0x200, file);
             MD5(data.dataFile, data.size, data.md5Res);
@@ -353,20 +372,23 @@ void uploadFile(FILE* file)
         }
     }
     free(blocks);
-    sendMessage(CLIENT_MSG::CANCEL_FILE_TRANSFER, NULL, 0);
+    sendMessage(CLIENT_MSG::END_FILE_TRANSFER, NULL, 0);
 }
 
-void downloadFile(FILE* file, int blockCount)
+void downloadFile(FILE* file, uint32_t blockCount)
 {
-    struct {
-        uint32_t blockNum;
-        uint16_t size;
-        uint8_t md5Res[16];
-        uint8_t dataFile[0x200];
-    } data;
-    char Buffer[BUFFER_SERVER_SIZE];
-    int range[2] = {0, blockCount};
-    sendMessage(CLIENT_MSG::ASK_BLOCK_RANGE, (char*)range, 8);
+    struct __attribute__((packed)) {
+        uint16_t msgCode;
+        struct __attribute__((packed)){
+            uint32_t blockNum;
+            uint16_t size;
+            uint8_t md5Res[MD5_DIGEST_LENGTH];
+            uint8_t dataFile[0x200];
+        } download;
+        char emptyData[BUFFER_SERVER_SIZE - sizeof(msgCode) - sizeof(download)];
+    } Buffer;
+    uint32_t range[2] = {0, htonl(blockCount)};
+    sendMessage(CLIENT_MSG::ASK_BLOCK_RANGE, (char*)range, sizeof(range));
     int i;
     uint8_t* blocks = (uint8_t*)malloc(blockCount); // 1 - bad, 0 - good
     uint8_t md5Res[MD5_DIGEST_LENGTH];
@@ -374,25 +396,26 @@ void downloadFile(FILE* file, int blockCount)
     while(flag)
     {
         i = recv(sock, (char*)&Buffer, sizeof(Buffer), 0);
-        memcpy(&data, Buffer + 2, i - 2);
-        data.blockNum = ntohl(data.blockNum);
-        data.size = ntohs(data.size);
-        MD5(data.dataFile, data.size, md5Res);
-        if(memcmp(data.md5Res, md5Res, MD5_DIGEST_LENGTH)) // not equal
+        Buffer.msgCode = ntohs(Buffer.msgCode);
+
+        Buffer.download.blockNum = ntohl(Buffer.download.blockNum);
+        Buffer.download.size = ntohs(Buffer.download.size);
+        MD5(Buffer.download.dataFile, Buffer.download.size, md5Res);
+        if(memcmp(Buffer.download.md5Res, md5Res, MD5_DIGEST_LENGTH)) // not equal
         {
-            data.blockNum = htonl(data.blockNum);
-            sendMessage(CLIENT_MSG::ASK_BLOCK, (char*)&data.blockNum, sizeof(data.blockNum)); // ask for block
+            Buffer.download.blockNum = htonl(Buffer.download.blockNum);
+            sendMessage(CLIENT_MSG::ASK_BLOCK, (char*)&Buffer.download.blockNum, sizeof(Buffer.download.blockNum)); // ask for block
         }
         else
         {
-            fseek(file, data.blockNum * 0x200, SEEK_SET);
-            i = fwrite(data.dataFile, 1, data.size, file);
-            blocks[data.blockNum] = 0;
+            fseek(file, Buffer.download.blockNum * 0x200, SEEK_SET);
+            i = fwrite(Buffer.download.dataFile, 1, Buffer.download.size, file);
+            blocks[Buffer.download.blockNum] = 0;
             flag = 0;
             for(i = 0; !flag && i < blockCount; i++)
                 flag |= blocks[i]; // only if the whole array it 0 (we finished) than flag will be 0
         }
     }
-
-    sendMessage(CLIENT_MSG::CANCEL_FILE_TRANSFER, NULL, 0);
+    free(blocks);
+    sendMessage(CLIENT_MSG::END_FILE_TRANSFER, NULL, 0);
 }
